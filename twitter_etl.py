54bb1c43699f376edc4e23f0b9ef10e9f30c2e0a
Abdal Khan <abdal_ijaz_k@hotmail.com>
2023-06-02 22:16:47 -0500

    update twitter_etl

--- twitter_etl.py
+++ twitter_etl.py
@@ -1,16 +1,32 @@
 import tweepy
 import configparser
+import json
 
-auth = tweepy.OAuth1UserHandler(
-   "API / Consumer Key here", "API / Consumer Secret here",
-   "Access Token here", "Access Token Secret here"
-)
-api = tweepy.API(auth)
-
-
-
+# parse config file to get twitter API secret keys
 parser = configparser.ConfigParser()
 parser.read('config.ini')
 
-SECRET = parser.get("reddit_config", "secret")
-CLIENT_ID = parser.get("reddit_config", "client_id")
+API_KEY = parser.get("twitter_config", "api_key")
+API_SECRET_KEY = parser.get("twitter_config", "api_secret_key")
+ACCESS_TOKEN = parser.get("twitter_config", "access_token")
+ACCESS_TOKEN_SECRET = parser.get("twitter_config", "access_token_secret")
+
+auth = tweepy.OAuthHandler(API_KEY, API_SECRET_KEY)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+api = tweepy.API(auth)
+
+tweets = api.user_timeline(screen_name='@elonmusk',
+                           # 200 is maximum allowed count
+                           count=2,
+                           include_rts=False,
+                           # necessary to keep full text otherwise
+                           # only first 140 word shown
+                           tweet_mode = 'extended')
+
+for tweet in tweets:
+    tweet_dict = tweet._json  # Convert tweet object to dictionary
+    formatted_json = json.dumps(tweet_dict, indent=4)
+    print(formatted_json)
+    print(tweet.id)
+    print(tweet_dict['id'])
