73388beeb84fa1c0c7caa4ea7b3f30029961537b
Abdal Khan <abdal_ijaz_k@hotmail.com>
2023-06-02 23:35:28 -0500

    convert api results to datafram

--- twitter_etl.py
+++ twitter_etl.py
@@ -1,9 +1,10 @@
 import tweepy
 import configparser
-import json
+import pandas as pd
 
 
 def api_connect(API_KEY, API_SECRET_KEY, ACCESS_TOKEN, ACCESS_TOKEN_SECRET):
+   '''This function creates and returns a twitter api connection'''
    auth = tweepy.OAuthHandler(API_KEY, API_SECRET_KEY)
    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
@@ -11,7 +12,7 @@ def api_connect(API_KEY, API_SECRET_KEY, ACCESS_TOKEN, ACCESS_TOKEN_SECRET):
    return api
 
 def get_access_keys(path):
-   # parse config file to get twitter API secret keys
+   '''This function parses config file in local/specified repository to fetch twitter API secret keys'''
    parser = configparser.ConfigParser()
    parser.read(path)
    API_KEY = parser.get("twitter_config", "api_key")
@@ -20,33 +21,60 @@ def get_access_keys(path):
    ACCESS_TOKEN_SECRET = parser.get("twitter_config", "access_token_secret")
    return API_KEY, API_SECRET_KEY, ACCESS_TOKEN, ACCESS_TOKEN_SECRET
 
+def create_dataframe(list_of_dictionaries):
+   '''This function converts the tweet object attributes list into a dataframe'''
+   df = pd.DataFrame(list_of_dictionaries)
+   print(df.head(5))
 
 
 def main():
-   # parse config file to get twitter API secret keys
-   
+
    '''************PARAMETERS***********'''
    SECRET_KEYS_PATH = 'config.ini'
+   search_query = "data engineering"  # Replace with your desired search query
+   num_tweets = 100  # Specify the number of tweets to retrieve
+   search_results = [] # Create a list to store the search results
    '''*********************************'''
-   
+
    # API_KEY, API_SECRET_KEY, ACCESS_TOKEN, ACCESS_TOKEN_SECRET = get_access_keys(SECRET_KEYS_PATH)
    # api = api_connect(API_KEY, API_SECRET_KEY, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
    # condensed version of above
 
-   api = api_connect(*get_access_keys('config.ini'))
+   api = api_connect(*get_access_keys(SECRET_KEYS_PATH))
 
-   tweets = api.user_timeline(screen_name='@elonmusk',
-                              # 200 is maximum allowed count
-                              count=2,
-                              include_rts=False,
-                              # necessary to keep full text otherwise
-                              # only first 140 word shown
-                              tweet_mode = 'extended')
+   # tweets = api.user_timeline(screen_name='@elonmusk',
+   #                            # 200 is maximum allowed count
+   #                            count=2,
+   #                            include_rts=False,
+   #                            # necessary to keep full text otherwise
+   #                            # only first 140 word shown
+   #                            tweet_mode = 'extended')
 
+   # for tweet in tweets:
+   #    tweet_dict = tweet._json  # Convert tweet object to dictionary
+   #    formatted_json = json.dumps(tweet_dict, indent=4)
+   #    print(formatted_json)
+
+   # Assuming you already have the 'api' object from the Tweepy API connection
+
+   # Perform the search
+   tweets = api.search_tweets(q=search_query, count=num_tweets)
+
+   # Process and print the search results
    for tweet in tweets:
-      tweet_dict = tweet._json  # Convert tweet object to dictionary
-      formatted_json = json.dumps(tweet_dict, indent=4)
-      print(formatted_json)
+      username = tweet.user.screen_name
+      tweet_text = tweet.text
+      created_at = tweet.created_at
+      location = tweet.user.location
+
+      search_results.append({
+                              "Username": username,
+                              "Tweet_text": tweet_text,
+                              "Created_at" : created_at,
+                              "Location": location                              
+                              })
+      
+      create_dataframe(search_results)
 
 
 
