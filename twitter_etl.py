fc46a7a26b5edff59c3bd3c1ac163f839623fe38
Abdal Khan <abdal_ijaz_k@hotmail.com>
2023-06-03 14:00:41 -0500

    fetched docker-compose file

--- twitter_etl.py
+++ twitter_etl.py
@@ -23,41 +23,26 @@ def get_access_keys(path):
 
 def create_dataframe(list_of_dictionaries):
    '''This function converts the tweet object attributes list into a dataframe'''
-   df = pd.DataFrame(list_of_dictionaries)
-   print(df.head(5))
+   return pd.DataFrame(list_of_dictionaries)
 
-
+def load_to_csv(extracted_df):
+   """Save extracted data to CSV file in /tmp folder"""
+   
+   extracted_df.to_csv('tmp/output.csv', index=False)
+   
+    
 def main():
 
    '''************PARAMETERS***********'''
    SECRET_KEYS_PATH = 'config.ini'
    search_query = "data engineering"  # Replace with your desired search query
-   num_tweets = 100  # Specify the number of tweets to retrieve
+   num_tweets = 100  # Specify the number of tweets to retrieve (maximum of 100)
    search_results = [] # Create a list to store the search results
    '''*********************************'''
 
-   # API_KEY, API_SECRET_KEY, ACCESS_TOKEN, ACCESS_TOKEN_SECRET = get_access_keys(SECRET_KEYS_PATH)
-   # api = api_connect(API_KEY, API_SECRET_KEY, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-   # condensed version of above
-
    api = api_connect(*get_access_keys(SECRET_KEYS_PATH))
 
-   # tweets = api.user_timeline(screen_name='@elonmusk',
-   #                            # 200 is maximum allowed count
-   #                            count=2,
-   #                            include_rts=False,
-   #                            # necessary to keep full text otherwise
-   #                            # only first 140 word shown
-   #                            tweet_mode = 'extended')
-
-   # for tweet in tweets:
-   #    tweet_dict = tweet._json  # Convert tweet object to dictionary
-   #    formatted_json = json.dumps(tweet_dict, indent=4)
-   #    print(formatted_json)
-
-   # Assuming you already have the 'api' object from the Tweepy API connection
-
-   # Perform the search
+   # Perform the search only against a sampling of recent Tweets published in the past 7 days
    tweets = api.search_tweets(q=search_query, count=num_tweets)
 
    # Process and print the search results
@@ -74,9 +59,9 @@ def main():
                               "Location": location                              
                               })
       
-      create_dataframe(search_results)
+      extracted_df = create_dataframe(search_results)
 
-
-
+      load_to_csv(extracted_df)
+      
 if __name__ == '__main__':
    main()
